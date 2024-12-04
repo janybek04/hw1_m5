db9cb1061545ec118ffaf0e42b05c6bfc276651b
janybek04 <storm20040609@gmail.com>
2024-12-04 14:35:08 +0600

    new commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import reducer from './redux/Reducer';
+
+const  store = createStore(reducer)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+        <App />
+    </Provider>
+);
